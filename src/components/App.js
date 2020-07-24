2acca2e98170d661237f81b2727679aada2732a4
chmelan <chmelan@comcast.net>
2020-07-24 09:09:04 -0400

    rewrote code to allow for error handling

--- src/components/App.js
+++ src/components/App.js
@@ -7,62 +7,48 @@ class App extends React.Component {
     super(props);
     this.state = { tempMode: "farenheit" };
   }
-  componentDidMount() {
-    this.getWeather("Lancaster");
-    this.getWeatherGif("rain");
-  }
+
   getWeather = async (location) => {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a3e0ba4476d1096ca94921c3346150b9`,
       { mode: "cors" }
     );
-
+    console.log(response);
+    if (!response.ok) {
+      this.updateWeatherError(true);
+      this.updateWeatherData(null);
+      return;
+    }
+    this.updateWeatherError(false);
     const weatherData = await response.json();
-    console.log(weatherData);
-
     this.updateWeatherData(weatherData);
-
-    return weatherData;
+    console.log(weatherData);
   };
-
-  getWeatherGif = async (weather) => {
-    const response = await fetch(
-      `https://api.giphy.com/v1/gifs/translate?api_key=AEepGv1Y4EwvuicBz5PnylRLHRS5Xruc&s=${weather}`,
-      { mode: "cors" }
-    );
-
-    const weatherGifData = await response.json();
-    console.log(weatherGifData.data.images.original.url);
-
-    this.updateWeatherGif(weatherGifData.data.images.original.url);
-    return weatherGifData.data.images.original.url;
-  };
-
-  getCompleteWeather = async (location) => {
-    const weatherData = await this.getWeather(location);
-    this.getWeatherGif(weatherData.weather[0].main);
-  };
-
-  updateWeatherData = (newWeatherData) => {
-    this.setState({ weatherData: newWeatherData });
-  };
-  updateWeatherGif = (newGif) => {
-    this.setState({ weatherGif: newGif });
-  };
-
+  updateWeatherError(error) {
+    this.setState({ weatherError: error });
+  }
+  updateWeatherData(data) {
+    this.setState({ weatherData: data });
+  }
   render() {
     return (
       <div className="app">
-        <WeatherForm getCompleteWeather={this.getCompleteWeather} />
+        <button onClick={() => this.getWeather("lancaster")}>Click ME</button>
+
+        <WeatherForm getCompleteWeather={this.getWeather} />
+        {this.state.weatherError ? (
+          <div className="weatherError">
+            There was a problem grabbing the weather
+          </div>
+        ) : null}
+
         {this.state.weatherData ? (
           <WeatherDisplay
             weatherData={this.state.weatherData}
             tempMode={this.state.tempMode}
             weatherGif={this.state.weatherGif}
           />
-        ) : (
-          <div> There was an error retrieving that city </div>
-        )}
+        ) : null}
       </div>
     );
   }
