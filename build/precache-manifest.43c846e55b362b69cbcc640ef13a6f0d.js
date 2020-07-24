5b2d49b9507f0c11629c3fac2f72281208975b10
chmelan <chmelan@comcast.net>
2020-07-24 14:08:22 -0400

    first production build

---
+++ build/precache-manifest.43c846e55b362b69cbcc640ef13a6f0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a565f4bf5184acf2a3382c00bb0a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68b2088777ce545efb4c",
+    "url": "/static/css/main.5f4a350b.chunk.css"
+  },
+  {
+    "revision": "d330fd7ecf1bfaa1cf58",
+    "url": "/static/js/2.a4f1cee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4f1cee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b2088777ce545efb4c",
+    "url": "/static/js/main.a19f699e.chunk.js"
+  },
+  {
+    "revision": "5cfe63f3d0aa65dc782b",
+    "url": "/static/js/runtime-main.dbafcd4d.js"
+  }
+]);
